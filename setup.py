3d9c89808a7e323444f27e394566840538c7217b
Robin Winslow <robin@robinwinslow.co.uk>
2017-08-01 14:31:12 +0100

    v1.0.2: Use latest views-from-yaml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='canonicalwebteam.yaml-redirects',
-    version='1.0.1',
+    version='1.0.2',
     author='Canonical Webteam',
     url='https://github.com/ubuntudesign/yaml-redirects',
     packages=[
@@ -14,6 +14,6 @@ setup(
     ),
     install_requires=[
         "Django >= 1.3",
-        "canonicalwebteam.views-from-yaml >= 0.2.1",
+        "canonicalwebteam.views-from-yaml >= 0.2.2",
     ],
 )
