df8e154a7c80c28b5ba316cc8ccfeb0cec6e3288
Robin Winslow <robin@robinwinslow.co.uk>
2017-11-22 18:16:21 +0000

    Parse any URL arguments into the target URL

--- canonicalwebteam/yaml_redirects/__init__.py
+++ canonicalwebteam/yaml_redirects/__init__.py
@@ -7,8 +7,8 @@ from django.shortcuts import redirect
 from canonicalwebteam.views_from_yaml import create_views_from_file
 
 
-def _redirect_to_target(request, target_url):
-    return redirect(target_url)
+def _redirect_to_target(request, target_url, *args, **kwargs):
+    return redirect(target_url.format(**kwargs))
 
 
 def create_views():
