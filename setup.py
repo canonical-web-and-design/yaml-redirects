35090de7c9386c2be5d37cafa7cfd3cb9ff92318
Robin Winslow <robin@robinwinslow.co.uk>
2018-03-20 19:16:13 +0000

    v1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='canonicalwebteam.yaml-redirects',
-    version='1.0.4',
+    version='1.0.5',
     author='Canonical Webteam',
     url='https://github.com/canonical-webteam/yaml-redirects',
     packages=[
